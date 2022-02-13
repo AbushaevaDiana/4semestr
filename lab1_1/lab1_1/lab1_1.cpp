7c8472831a76ec118e01f3da4d7af77e0952644b
Диана Абушаева <abu13.di@gmail.com>
2022-02-13 20:40:19 +0300

    первая лаба, проба

---
+++ lab1_1/lab1_1/lab1_1.cpp
@@ -0,0 +1,48 @@
+﻿// lab1_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+using namespace std;
+
+int main(int argc, char* argv[])
+
+{
+	setlocale(LC_ALL, "Russian");
+	
+	//Открытие файлов
+	string file = argv[1];
+	ifstream fileIn;
+	fileIn.open(file);
+	ofstream fileOut;
+	fileOut.open("../output.txt");
+
+	if (fileIn.is_open() && fileOut.is_open())
+	{
+		string strIn = argv[2];
+		int n = 1;
+		n =	strIn.length();
+		char* strArray = new char[n];
+
+		if (fileIn.peek() != EOF) 
+		{
+			fileIn >> strArray;
+			fileOut << strArray;
+		};
+	}
+	else
+		cout << "Ошибка открытия файла!" << endl;
+
+	//Закрытие файлов
+	fileIn.close();
+	fileOut.close();
+
+
+	return 0;
+
+}
